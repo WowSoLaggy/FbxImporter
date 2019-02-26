2dc39b9e5e4c364b0afdfd4cd5b84747d73c0b9b
WowSoLaggy <dohxehapo@gmail.com>
2019-02-26 23:22:07 +0300

    (=) FbxImporter project creation

---
+++ FbxImporter/FbxImporter.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "FbxImporter.h"
+
+
+void loadFromFile(const std::wstring& i_fileName)
+{
+}
---
+++ inc/FbxImporter.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <string>
+
+
+void loadFromFile(const std::wstring& i_fileName);
