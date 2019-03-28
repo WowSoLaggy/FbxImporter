f647a1291023f645014f70248c0d56f594011ead
WowSoLaggy <dohxehapo@gmail.com>
2019-03-28 22:21:35 +0300

    (=) Formatting

--- inc/Property.h
+++ inc/Property.h
@@ -37,15 +37,15 @@ const std::unordered_map<PropertyType, int> PropertyTypeToTypeSizeMap
 {
   { PropertyType::Bool,   1 },{ PropertyType::BoolArray,    1 },
 
-{ PropertyType::Short,  2 },
-{ PropertyType::Int,    4 },{ PropertyType::IntArray,     4 },
-{ PropertyType::Long,   8 },{ PropertyType::LongArray,    8 },
+  { PropertyType::Short,  2 },
+  { PropertyType::Int,    4 },{ PropertyType::IntArray,     4 },
+  { PropertyType::Long,   8 },{ PropertyType::LongArray,    8 },
 
-{ PropertyType::Float,  4 },{ PropertyType::FloatArray,   4 },
-{ PropertyType::Double, 8 },{ PropertyType::DoubleArray,  8 },
+  { PropertyType::Float,  4 },{ PropertyType::FloatArray,   4 },
+  { PropertyType::Double, 8 },{ PropertyType::DoubleArray,  8 },
 
-{ PropertyType::String, 1 },
-{ PropertyType::Raw,    1 },
+  { PropertyType::String, 1 },
+  { PropertyType::Raw,    1 },
 };
 
 enum class PropertySerializationType
@@ -59,21 +59,21 @@ const std::unordered_map<PropertyType, PropertySerializationType>
 PropertyTypeToSerializationTypeMap
 {
   { PropertyType::Bool,         PropertySerializationType::Value },
-{ PropertyType::BoolArray,    PropertySerializationType::EncodedArray },
+  { PropertyType::BoolArray,    PropertySerializationType::EncodedArray },
 
-{ PropertyType::Short,        PropertySerializationType::Value },
-{ PropertyType::Int,          PropertySerializationType::Value },
-{ PropertyType::IntArray,     PropertySerializationType::EncodedArray },
-{ PropertyType::Long,         PropertySerializationType::Value },
-{ PropertyType::LongArray,    PropertySerializationType::EncodedArray },
+  { PropertyType::Short,        PropertySerializationType::Value },
+  { PropertyType::Int,          PropertySerializationType::Value },
+  { PropertyType::IntArray,     PropertySerializationType::EncodedArray },
+  { PropertyType::Long,         PropertySerializationType::Value },
+  { PropertyType::LongArray,    PropertySerializationType::EncodedArray },
 
-{ PropertyType::Float,        PropertySerializationType::Value },
-{ PropertyType::FloatArray,   PropertySerializationType::EncodedArray },
-{ PropertyType::Double,       PropertySerializationType::Value },
-{ PropertyType::DoubleArray,  PropertySerializationType::EncodedArray },
+  { PropertyType::Float,        PropertySerializationType::Value },
+  { PropertyType::FloatArray,   PropertySerializationType::EncodedArray },
+  { PropertyType::Double,       PropertySerializationType::Value },
+  { PropertyType::DoubleArray,  PropertySerializationType::EncodedArray },
 
-{ PropertyType::String,       PropertySerializationType::Array },
-{ PropertyType::Raw,          PropertySerializationType::Array },
+  { PropertyType::String,       PropertySerializationType::Array },
+  { PropertyType::Raw,          PropertySerializationType::Array },
 };
 
 struct Property
